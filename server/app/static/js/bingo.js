4b54c1a60f193fb11b175ee5379f69257defb706
RedBug312 <redbug312@gmail.com>
2020-08-19 15:22:25 +0800

    Ensure a bingo row in draw animation

--- server/app/static/js/bingo.js
+++ server/app/static/js/bingo.js
@@ -1,21 +1,38 @@
 Array.prototype.sample = function(){
   return this[Math.floor(Math.random() * this.length)];
 };
-var colors = ['rgba(62,65,92,.8)',
-              'rgba(244,152,27,.8)'];
+
+var CENTRAL = [24];
+var SURROUNDS = [
+   8,  9, 10, 11, 12,
+  15, 16, 17, 18, 19,
+  22, 23,     25, 26,
+  29, 30, 31, 32, 33,
+  36, 37, 38, 39, 40,
+];
+var BINGOS = [
+  [ 8,  9, 10, 11, 12],
+  [15, 16, 17, 18, 19],
+  [29, 30, 31, 32, 33],
+  [36, 37, 38, 39, 40],
+  [ 8, 15, 22, 29, 36],
+  [ 9, 16, 23, 30, 37],
+  [11, 18, 25, 32, 39],
+  [12, 19, 26, 33, 40],
+];
+var COLORS = [
+  'rgba(62,65,92,.8)',
+  'rgba(244,152,27,.8)'
+];
 
 $(document).ready(function(){
 
   var canvas = document.querySelector('.canvas');
-  var grid = document.querySelectorAll('.grid');
-  var central = grid[24];
-  var surrounds = [
-     8,  9, 10, 11, 12,
-    15, 16, 17, 18, 19,
-    22, 23,     25, 26,
-    29, 30, 31, 32, 33,
-    36, 37, 38, 39, 40,
-  ].map(i => grid[i]);
+  var square = document.querySelectorAll('.square');
+
+  var central = CENTRAL.map(i => square[i]);
+  var surrounds = SURROUNDS.map(i => square[i]);
+  var bingo = new Set(BINGOS.sample().map(i => square[i]));
 
   /* burst */
 
@@ -26,12 +43,13 @@ $(document).ready(function(){
   })
   .add({  // Highlight central border-color
     targets: central,
-    borderColor: () => ['rgba(100,100,100,.8)', colors.sample()],
+    borderColor: () => ['rgba(100,100,100,.8)', COLORS.sample()],
     duration: 1000,
   })
 
+  console.log(bingo);
   surrounds.forEach(item => {
-    var color = colors.sample();
+    var color = COLORS.sample();
     burst
     .add({  // Highlight surrounding border-color
       targets: item,
@@ -40,34 +58,36 @@ $(document).ready(function(){
     }, 0)
     .add({  // Highlight surrounding background-color
       targets: item,
-      backgroundColor: Math.random() < 0.7 ? color : 'transparent',
-      delay: anime.random(200, 1000),
-      duration: 1000,
+      backgroundColor: bingo.has(item) || Math.random() < 0.6
+                       ? color : 'transparent',
+      delay: anime.random(0, 800),
+      duration: 600,
     }, 1000)
   });
 
   burst
-  .add({  // Rotate and scatter surrounding grids
+  .add({  // Rotate and scatter surrounding squares
     targets: surrounds,
     opacity: .3,
     translateX: () => anime.random(-8, 8) * 2.5 + 'rem',
     rotateZ: () => anime.random(-4, 4) * 15,
-    delay: 500,
-    duration: 1000,
+    delay: 400,
+    duration: 800,
   })
-  .add({  // Scale up central grid
+  .add({  // Scale up central square
     targets: central,
     opacity: .5,
-    scale: 5,
-    borderWidth: [4/16 + 'rem', 2/16 + 'rem'],
-    duration: 1000,
+    scale: 6,
+    borderWidth: [4/16 + 'rem', 1/16 + 'rem'],
+    boxShadow: '0 0 0 0',
+    duration: 800,
     complete: () => last.play(),
-  }, '-=1000')
+  }, '-=800')
   .add({  // Fade in winner name
     targets: 'article#bingo .winner',
     opacity: [0, 1],
-    easing: 'easeInOutCubic',
-    duration: 1000,
+    // easing: 'easeInOutCubic',
+    duration: 800,
   });
 
   /* last */
@@ -76,13 +96,13 @@ $(document).ready(function(){
     autoplay: false,
     loop: true,
   })
-  .add({  // Keep rotating central grid
+  .add({  // Keep rotating central square
     targets: central,
     keyframes: [
       {scale: 1, duration: 1},
-      {scale: [5, 5], duration: 9999},
+      {scale: [6, 6], duration: 9999},
     ],
-    rotateZ: 360,
+    rotateZ: 180,
     easing: 'linear',
     duration: 10000,
   });
