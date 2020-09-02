1d8a023ac07829a916fc33ca857e66ffdd3321d1
RedBug312 <redbug312@gmail.com>
2020-09-02 23:24:31 +0800

    Fix over-consume dataframe rows in each bingo draw

--- server/app/views/bonus.py
+++ server/app/views/bonus.py
@@ -10,22 +10,19 @@ bonus = Blueprint('bonus', __name__)
 @bonus.route('/bonus/draw')
 def draw():
     try:
-        i = request.args.get('i', default=0, type=int)
         seed = request.args.get('seed', type=int)
     except ValueError:
         return abort(401)  # When argument failed casting
     if not seed:
         rand = random.randint(100000, 999999)
-        return redirect(url_for('.draw', i=0, seed=rand))
+        return redirect(url_for('.draw', seed=rand))
 
-    winners = db.draw(n=2, seed=seed, instagram=True)
+    winners = db.draw(n=1, seed=seed, instagram=True)
     if winners.empty:
         return abort(404)  # When no one can be drawn
-    elif i >= winners.shape[0]:
-        return redirect(url_for('.draw'))
 
-    link = url_for('.draw', i=(i + 1), seed=seed)
-    instagram = '@%s' % winners.iloc[i].INST
+    link = url_for('.draw', seed=seed)
+    instagram = '@%s' % winners.iloc[0].INST
     return render_template('bingo.pug', link=link, name=instagram)
 
 
